b2f9f8cabfe9ebc448827104ccc963f98275c515
Dan Hog <dan24613@outlook.de>
2019-09-27 20:23:58 +0200

    Projektdateien hinzufügen.

---
+++ Raytracing/Class1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Raytracing
+{
+    class Ray
+    {
+        
+    }
+
+    abstract class Object
+    {
+
+
+        public abstract 
+    }
+
+    class Plane : Object
+    {
+
+    }
+
+    class Sphere : Object
+    {
+        Vec
+    }
+
+    class Vector
+    {
+        public int Length => arr.Length;
+        public double[] arr;
+
+        public Vector(int length)
+        {
+            arr = new double[length];
+        }
+
+        public double this[int i]
+        {
+            get => arr[i];
+            set => arr[i] = value;
+        }
+
+        public static Add(Vector a, Vector b)
+        {
+            if (a.Length != b.Length)
+                throw new Exception();
+            Vector c = new Vector(a.Length);
+        }
+    }
+}
